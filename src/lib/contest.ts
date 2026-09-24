ca65c9a5d1f18075f8150d16b270aa262fe97999
M7zain <zoudne@gmail.com>
2026-09-24 10:36:06 +0300

    remove prize

--- src/lib/contest.ts
+++ src/lib/contest.ts
@@ -1,7 +1,6 @@
 export const CONTEST = {
   postUrl: 'https://www.instagram.com/p/Ddn4ZSqoPCT/',
   shortcode: 'Ddn4ZSqoPCT',
-  prize: 'كوبون مشتريات بقيمة 5 د.ك',
   home: 'السعودية',
   away: 'الكويت',
 }
